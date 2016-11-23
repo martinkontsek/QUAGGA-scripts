52b693967e7b45f7d058c4cad7490b18c67d1160
Martin Kontsek <martinkontsek@gmail.com>
2016-11-22 23:34:42 -0800

    Fix build script.

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#
+# build.sh
+# Copyright (c) 2016
+#
+# Author:
+#   Martin Kontsek
+#
+# This file is part of QUAGGA-scripts
+#
+# QUAGGA-scripts is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any
+# later version.
+#
+# QUAGGA-scripts is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+#
+# Quagga builder script
+#
 
 # Colors
 ESC_SEQ="\x1b["
@@ -74,7 +96,7 @@ cecho "************************************************" $YELLOW
 echo -e "$GREEN"
 read -p "  Press key to continue..."
 echo -e "$RESET"
-make install
+sudo make install
 
 
 cecho "************************************************************************" $YELLOW
